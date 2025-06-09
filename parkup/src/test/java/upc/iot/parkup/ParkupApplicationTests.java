3a3ee1c709fdc6cd00331a8069dc6424d15e43de
Ariana Vargas Revollé <105668599+vargas3470@users.noreply.github.com>
2025-06-09 18:00:46 -0500

    Initial commit

---
+++ parkup/src/test/java/upc/iot/parkup/ParkupApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.iot.parkup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
